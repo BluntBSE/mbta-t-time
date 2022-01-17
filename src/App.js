9e3fc330cd4930187d9dc56ef6a0c5fb5b06b39d
BluntBSE <ram.news1@gmail.com>
2022-01-17 16:22:08 -0500

    Initital Commit
    Setting up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Dropdown, Selection } from 'react-dropdown-now';
+import 'react-dropdown-now/style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <header className="site-header">
+      Welcome to T-Time
+        
       </header>
+
+      <div className="site-wrap">
+
+        <div className="stop-panel">
+          <div className="stop-banner">I am the banner</div>
+          <div className="stop-controls">
+          <Dropdown
+          placeholder="Select an option"
+          options={['one', 'two', 'three']}
+          value="one"
+          onChange={(value) => console.log('change!', value)}
+          onSelect={(value) => console.log('selected!', value)} // always fires once a selection happens even if there is no change
+          onClose={(closedBySelection) => console.log('closedBySelection?:', closedBySelection)}
+          onOpen={() => console.log('open!')}
+           />
+          <Dropdown
+          placeholder="Select an option"
+          options={['one', 'two', 'three']}
+          value="one"
+          onChange={(value) => console.log('change!', value)}
+          onSelect={(value) => console.log('selected!', value)} // always fires once a selection happens even if there is no change
+          onClose={(closedBySelection) => console.log('closedBySelection?:', closedBySelection)}
+          onOpen={() => console.log('open!')}
+           />                 
+          <Dropdown
+          placeholder="Select an option"
+          options={['one', 'two', 'three']}
+          value="one"
+          onChange={(value) => console.log('change!', value)}
+          onSelect={(value) => console.log('selected!', value)} // always fires once a selection happens even if there is no change
+          onClose={(closedBySelection) => console.log('closedBySelection?:', closedBySelection)}
+          onOpen={() => console.log('open!')}
+           />
+
+           <div className = "go-button">Go</div>
+           
+           </div>
+          <div className="stop-content">
+              <div className="stop-section">
+              <h2>Outbound</h2>
+              <p>The next train to Alewife arrives in 10 Minutes and 58 seconds.</p>
+              <p>The following train to Alewife arrives in 20 Minutes and 48 seconds.</p>
+              </div>
+              <div className="stop-section">
+              <h2>Inbound</h2>
+              <p>The next train to Braintree arrives in 10 Minutes and 58 seconds.</p>
+              <p>The following train to Ashmont arrives in 20 Minutes and 48 seconds.</p>
+              </div>
+          
+          </div>
+        </div>
+        <div className="alert-panel">
+          <div className="stop-banner">I am the alert banner</div>
+          <div className="alert-content">
+          I am the alert content
+          </div>
+
+        </div>
+
+
+      </div>
     </div>
   );
 }
